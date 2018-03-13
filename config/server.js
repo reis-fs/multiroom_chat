d6e1defed588e966d44a50411da7821bc205bad1
Felipe Reis <coder.felipe@gmail.com>
2018-03-12 22:43:03 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,18 +1,26 @@
 // importar modulos
 var express = require('express');
+var path = require('path');
 var consign = require('consign');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
+
+express()
+  .use(express.static(path.join(__dirname, './app/public')))
+  .set('views', path.join(__dirname, './app/views'))
+  .set('view engine', 'ejs')
+  //.get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // iniciar o objeto do express
 var app = express();
 
 /* configurar o ejs, setar variaveis 'view engine' e 'views' do express */
-app.set('view engine', 'ejs');
-app.set('views', './app/views');
+//app.set('view engine', 'ejs');
+//app.set('views', './app/views');
 
 // configurar middleware express.static
-app.use(express.static('./app/public'));
+//app.use(express.static('./app/public'));
 
 // configurar middleware body-parser
 app.use(bodyParser.urlencoded({extended:true}));

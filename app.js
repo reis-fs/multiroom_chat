b051dd64ccc462a83ede8430f098df429a49b12c
Felipe Reis <coder.felipe@gmail.com>
2018-03-12 22:31:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 //importar as configurações do servidor
 var app = require('./config/server');
-
+var PORT = process.env.PORT || 5000
 //parametrizar porta de escuta
-app.listen(3000, function(){
-  console.log('Servidor OK');
-});
+applisten(PORT, () => console.log(`Listening on ${ PORT }`))

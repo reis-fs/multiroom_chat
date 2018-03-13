bce97dcdee345a7b4b13a76b44f3b2145946388f
Felipe Reis <coder.felipe@gmail.com>
2018-03-12 22:34:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,4 @@
 var app = require('./config/server');
 var PORT = process.env.PORT || 5000
 //parametrizar porta de escuta
-applisten(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
